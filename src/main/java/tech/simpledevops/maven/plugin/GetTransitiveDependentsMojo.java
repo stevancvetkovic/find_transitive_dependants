1d5f8afa9ac49941a2c787377e1954f9204e6ce9
Stevan Cvetkovic <stevan.cvetkovic@endava.com>
2019-10-15 13:27:53 +0200

    FindBugs fixes

--- src/main/java/tech/simpledevops/maven/plugin/GetTransitiveDependentsMojo.java
+++ src/main/java/tech/simpledevops/maven/plugin/GetTransitiveDependentsMojo.java
@@ -1,36 +1,40 @@
 package tech.simpledevops.maven.plugin;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 import java.util.ArrayList;
 import java.util.Collection;
-import java.lang.Exception;
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.HashSet;
 import java.util.List;
-
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.plugins.annotations.Component;
 
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.execution.ProjectDependencyGraph;
 import org.apache.maven.graph.DefaultProjectDependencyGraph;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectBuilder;
 import org.apache.maven.project.ProjectBuildingException;
 import org.apache.maven.project.ProjectBuildingRequest;
 import org.apache.maven.project.ProjectBuildingResult;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 
 import org.eclipse.aether.RepositorySystemSession;
 
-@Mojo(name = "get-transitive-deps", requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
+@Mojo(name = "get-transitive-deps",
+        requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME,
+        requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
+
 public class GetTransitiveDependentsMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
@@ -49,12 +53,12 @@ public class GetTransitiveDependentsMojo extends AbstractMojo {
     private Collection<MavenProject> allMavenProjects = new HashSet<MavenProject>();
 
     private Model readPomFile(File basedir) throws Exception {
-        Model model = new Model();
         MavenXpp3Reader reader = new MavenXpp3Reader();
 
         File pom = new File(basedir, "pom.xml");
-        BufferedReader in = new BufferedReader(new FileReader(pom));
-        model = reader.read(in);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(new FileInputStream(pom), "UTF-8"));
+        Model model = reader.read(in);
         model.setPomFile(pom);
 
         return model;
@@ -99,7 +103,9 @@ public class GetTransitiveDependentsMojo extends AbstractMojo {
                 System.out.println("Transitive dependants of " + project.getArtifactId());
                 List<MavenProject> transitiveDependants = findTransitiveDependants(project);
 
-                for (MavenProject dependentProject : transitiveDependants) System.out.println("\t" + dependentProject.getArtifactId());
+                for (MavenProject dependentProject : transitiveDependants) {
+                    System.out.println("\t" + dependentProject.getArtifactId());
+                }
                 System.out.println("\n");
             }
         } catch (Exception e) {
